8f9a2e4a3d3da03db80afb3a947216242c31609e
hailv <zefredzocohen@gmail.com>
2016-11-16 00:42:12 +0700

    update

---
+++ database/news.sql
@@ -0,0 +1,20 @@
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for user
+-- ----------------------------
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `username` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `role` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of user
+-- ----------------------------
+INSERT INTO `user` VALUES ('1', 'admin', 'admin@gmail.com', 'e10adc3949ba59abbe56e057f20f883e', '1');
+SET FOREIGN_KEY_CHECKS=1;
